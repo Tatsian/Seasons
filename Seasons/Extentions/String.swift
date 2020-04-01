658eed671168bcbc1c79bc385cc160ae267eafbd
Tatsiana Pasiukova <tatiana.will.read@gmail.com>
2020-04-01 11:44:04 +0300

    extension was changed

--- Seasons/Extentions/String.swift
+++ Seasons/Extentions/String.swift
@@ -21,12 +21,4 @@ extension String {
     func localized(withArguments arguments: CVarArg..., comment: String = "") -> String {
         return String(format: self.localized(withComment: comment), arguments: arguments)
     }
-    
-    func isValidEmail() -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._-]{2,64}+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,127}"
-        
-        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with:self)
-    }
-    
 }
